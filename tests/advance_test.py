10db1065635424ba181331f0fca9a5c7fbfd15d7
Thanh Cao <caoduythanhcantho@gmail.com>
2022-03-20 17:14:41 +0700

    Add test cases

--- tests/advance_test.py
+++ tests/advance_test.py
@@ -2,14 +2,6 @@ import unittest
 from crdp import rdp
 
 
-class SimpleTest(unittest):
-    def test_empty_line(self):
-        p = rdp([], 0)
-        self.eq
-
-
-    def run_1_line(self):
-        points = [
-            [0, 5], [5, 5], [10, 5] [15, 5]
-        ]
-        rdp()
+class AdvanceTest(unittest.TestCase):
+    def test_8_ring(self):
+        pass
---
+++ tests/simple_test.py
@@ -0,0 +1,31 @@
+import unittest
+from crdp import rdp
+
+
+class SimpleTest(unittest.TestCase):
+    def test_empty_line(self):
+        result = rdp([], 0)
+        self.assertEqual(len(result), 0)
+
+    def run_1_line(self):
+        points = [[0, 5], [5, 5], [10, 5], [15, 5]]
+        result = rdp(points, 0.1)
+        self.assertEqual(len(result), 2)
+        self.assertListEqual(result, [[0, 5], [15, 5]])
+
+    def run_zig_zag_line(self):
+        points = [[0, 5], [5, 7], [10, 3], [15, 5]]
+        result = rdp(points, 0.1)
+        self.assertEqual(len(result), 4)
+        self.assertListEqual(result, [[0, 5], [5, 7], [10, 3], [15, 5]])
+
+        points = [[0, 5], [5, 7], [10, 3], [15, 5]]
+        result = rdp(points, 0.7)
+        self.assertEqual(len(result), 2)
+        self.assertListEqual(result, [[0, 5], [15, 5]])
+
+    def test_a_ring(self):
+        points = [[0, 0], [5, 0], [5, 5], [0, 5], [0, 0]]
+        result = rdp(points, 0.1)
+        self.assertEqual(len(result), 2)
+        self.assertListEqual(result, [[0, 0], [0, 0]])
